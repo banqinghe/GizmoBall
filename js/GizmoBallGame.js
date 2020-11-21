0d370e60c65ada8430890e63064cd87b92a4a1eb
banqinghe <qingheban@qq.com>
2020-11-21 16:03:05 +0800

    添加关于界面

--- js/GizmoBallGame.js
+++ js/GizmoBallGame.js
@@ -5,8 +5,10 @@ export default class GizmoBallGame{
     this.gameBoard = new GameBoard();
 
     this.dragListener();
+    this.topbarListener();
   }
 
+  // 为组件栏添加拖动监听
   dragListener() {
     // 传递拖动item的类型
     document.querySelectorAll('.item').forEach(element => {
@@ -14,5 +16,32 @@ export default class GizmoBallGame{
         e.dataTransfer.setData('text', element.className.split(' ')[0]);
       })
     })
+  }
+
+  // 为顶栏添加按钮监听
+  topbarListener() {
+    const fileButton = document.querySelector('#file-button');
+    const aboutButton = document.querySelector('#about-button');
+    const closeAbout = document.querySelector('.about-box-top span');
+    
+    fileButton.addEventListener('click', e => {
+      console.log('file');
+    });
+
+    const aboutPage = document.querySelector('.about-page');
+    const aboutBox = document.querySelector('.about-box');
+
+    // 弹出信息框
+    aboutButton.addEventListener('click', e => {
+      aboutPage.style.display = 'flex';
+      // 渲染完成后展示，开始过渡动画
+      setTimeout(() => aboutBox.classList.add('show-box'), 0);
+    });
+
+    // 关闭信息框
+    closeAbout.addEventListener('click', e => {
+      aboutBox.classList.remove('show-box');
+      aboutPage.style.display = 'none'
+    });
   }
 }
