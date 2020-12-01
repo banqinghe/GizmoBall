064be8b65e7d4994a63b2ac3ef204c6726ad0c74
KamePan <1355887943@qq.com>
2020-12-01 19:14:26 +0800

    增加了添加和放大时的检测功能

--- js/Border.js
+++ js/Border.js
@@ -15,8 +15,6 @@ export default class Border extends Item{
         this.lineList.push(new Line(this.x, this.y, this.size, Math.acos(0)));
         this.lineList.push(new Line(this.x, this.y + this.height, this.size, 0));
         this.lineList.push(new Line(this.x + this.width, this.y, this.size, Math.acos(0)));
-
-        console.log(this.lineList);
     }
 
 }
